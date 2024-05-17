705bde5b45a8fb2e164e86d5b313ff4302af53bf
hzrd149 <github@hzrd149.com>
2024-05-17 12:19:57 -0500

    add types to desktop config add missing dependencies

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,7 +4,7 @@ import { randomBytes } from 'crypto';
 
 import JSONFile from '../helpers/jsonfile.js';
 
-type ConfigDefaults = {
+type DesktopConfig = {
 	auth: string;
 	nodePort: number;
 	activeIdentity: string;
@@ -13,14 +13,14 @@ type ConfigDefaults = {
 
 const DEFAULT_FILENAME = 'config.json';
 
-const DEFAULT_VALUES: ConfigDefaults = {
+const DEFAULT_VALUES: DesktopConfig = {
 	auth: randomBytes(20).toString('hex'),
 	nodePort: 2012,
 	activeIdentity: '',
 	NIP07TrustedDomains: ['local'],
 };
 
-export default class Config extends JSONFile {
+export default class Config extends JSONFile<DesktopConfig> {
 	constructor(filename = DEFAULT_FILENAME) {
 		super(path.join(app.getPath('userData'), filename), DEFAULT_VALUES);
 	}
